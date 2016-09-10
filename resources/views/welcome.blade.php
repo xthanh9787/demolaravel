15d94795e566a5885c26f624f3f35e48a8b3fd34
thanhpx6653 <thahpx6653@seta-asia.com.vn>
2016-09-10 09:49:29 +0700

    create branch develop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,10 +72,26 @@
                     <a href="{{ url('/register') }}">Register</a>
                 </div>
             @endif
-
-            <div class="content">
+            <div class="content PC">
                 <div class="title m-b-md">
-                    Laravel
+                    <p>{{$a = 1}}</p>
+                    <p>Laravel weaadfghjkl;''</p>
+
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+            <div class="content mobile">
+                <div class="title m-b-md">
+                    <p>Laravel weaadfghjkl;''</p>
+                    <p>Laravel weaadfghjkl;''</p>
+
                 </div>
 
                 <div class="links">
